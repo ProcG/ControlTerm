cb0a2e0c2abb5533c1acaad21cef2d8ab8c7e881
Aluno <Aluno@Digital.net>
2018-05-29 18:05:07 -0300

    relatoio

---
+++ Cali_Imperium/Cali_Imperium/Sistema/Relatorio/Relatorio.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cali_Imperium.Sistema.Relatorio
+{
+    public class Relatorio
+    {
+    }
+}
